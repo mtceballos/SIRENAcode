c43b7a528748b45617e93841ccefb8de4787be4b
Maite Ceballos <ceballos@ifca.unican.es>
2017-06-29 20:21:25 +0200

    Add behaviour of secondaries w/o detection

--- ERESOL/PAIRS/secondaries.R
+++ ERESOL/PAIRS/secondaries.R
@@ -2,14 +2,16 @@
 # 
 # Plot EnergyReconsCorrected vs EnergyInputSecondary for different separations of Secondary pulse  (no Sec Detection)
 # Plot EnergyReconsCorrected vs EnergyInputSecondary for different separations of Secondary pulse  (Sec Detection)
-
 setwd("/home/ceballos/INSTRUMEN/EURECA/ERESOL/PAIRS/eresolLPA2shunt")
+pdf("../NoSecondariesDetection.pdf")
+
 library(rjson)
 library(Hmisc)
 nSamples <- "4096"
 pulseLength <- "4096"
 nSimPulses <- "2000"
 primEnergy <- 6 # keV
+singlePrimEnergy <- 5.1914 # energy of 6 keV single pulses recons with OPTFILT 1 keV
 sepsStr <- c("00005","00010", "00020", "00045")
 seps <- as.numeric(sepsStr)
 secEnergies <- c(0.2, 0.5, 1, 2, 3, 4, 5, 6, 7)
@@ -17,8 +19,10 @@ detectSPdirs <- c("nodetSP","detSP")
 detectSPdirs <- c("nodetSP")
     
 energyCorrPrim    <- matrix(NA,nrow=length(seps), ncol=length(secEnergies))
+energyUnCorrPrim    <- matrix(NA,nrow=length(seps), ncol=length(secEnergies))
 ebiasCorr         <- matrix(NA,nrow=length(seps), ncol=length(secEnergies))
-accuracy          <- matrix(NA,nrow=length(seps), ncol=length(secEnergies))
+ebiasUnCorr       <- matrix(NA,nrow=length(seps), ncol=length(secEnergies))
+diffPairSingle    <- matrix(NA,nrow=length(seps), ncol=length(secEnergies))
 fwhmPrimGAINCORRE <- matrix(NA,nrow=length(seps), ncol=length(secEnergies))
 fwhmPrimGAINCORREErr <- matrix(NA,nrow=length(seps), ncol=length(secEnergies))
 TRIGG <- "" # or "_NTRIGG"
@@ -28,17 +32,15 @@ labels <- character(length(seps))
 
 # READ BIAS 
 # =========
-#plot(secEnergies,seq(6.0,6.5,length.out = length(secEnergies)), xlab="Secondaries Energies (keV)",
-#                     ylab="(Mean) Primary Energies (keV; gain Corr)", type="n", main="Reconstruction of 6 keV pulses")
-plot(secEnergies,seq(0.1,3000.,length.out = length(secEnergies)), xlab="Secondaries Energies (keV)",
-     ylab="Bias for Primary (eV; gain Corr)", type="n", main="Reconstruction of 6 keV pulses")
-#plot(secEnergies,seq(1.8,2.5,length.out = length(secEnergies)), xlab="Secondaries Energies (keV)",
-#     ylab="FWHM Primary (eV; gain Corr)", type="n", main="Reconstruction of 6 keV pulses")
-
-abline(v=secEnergies, lty=2, col="gray")
 
 for (id in 1:length(detectSPdirs)){
     SPdir <- detectSPdirs[id]
+    plot(secEnergies,seq(0.,0.8,length.out = length(secEnergies)), xlab="Secondaries Energies (keV)",
+         ylab="(Eprim(pair)-Eprim(single))/EprimSingle", type="n", 
+         main=paste("Reconstruction of 6 keV pulses \n Secondaries (",SPdir,")",sep=""))
+    
+    abline(v=secEnergies, lty=2, col="gray")
+    
     for (is in 1:length(seps)){
         for (ie in 1:length(secEnergies)){
             eresolFile <- paste(SPdir,"/eresol_",nSimPulses,"p_SIRENA",nSamples,"_pL",pulseLength,
@@ -49,14 +51,16 @@ for (id in 1:length(detectSPdirs)){
                 idxSep <- which(sapply(jsondata,function(x) x$separation)==sepsStr[is])
                 if(length(idxSep)>0){
                     ebiasCorr[is,ie] <- as.numeric(jsondata[[idxSep]]$biasEreal$all) #eV
+                    ebiasUnCorr[is,ie] <- as.numeric(jsondata[[idxSep]]$biasErecons$all) #eV
                     energyCorrPrim[is,ie] <- ebiasCorr[is,ie]/1000. + primEnergy
-                    accuracy[is,ie] <- (ebiasCorr[is,ie]/1000. )/primEnergy
+                    energyUnCorrPrim[is,ie] <- ebiasUnCorr[is,ie]/1000. + primEnergy
+                    diffPairSingle[is,ie] <- (energyUnCorrPrim[is,ie]-singlePrimEnergy)/singlePrimEnergy
                     fwhmPrimGAINCORRE[is,ie] <- as.numeric(jsondata[[idxSep]]$fwhmEreal$all)
                     fwhmPrimGAINCORREErr[is,ie] <- as.numeric(jsondata[[idxSep]]$fwhmEreal_err$all)
                     
                 }else{
                     energyCorrPrim[is,ie] <- NaN
-                    accuracy[is,ie] <- NaN
+                    diffPairSingle[is,ie] <- NaN
                     fwhmPrimGAINCORRE[is,ie] <- NaN
                 }
                 
@@ -67,8 +71,8 @@ for (id in 1:length(detectSPdirs)){
             } # file exists
         }# secEnergies [ie]
         #lines(secEnergies, energyCorrPrim[is,], col=colores[is],lty=id, typ="b",cex=0.5)
-        lines(secEnergies, ebiasCorr[is,], col=colores[is],lty=id, typ="b",cex=0.5)
-        abline(h=0.014)
+        #
+        lines(secEnergies, diffPairSingle[is,], col=colores[is],lty=id, typ="b",cex=0.5)
         #lines(secEnergies, fwhmPrimGAINCORRE[is,], col=colores[is],lty=id, typ="b",cex=0.5)
         #errbar(secEnergies,fwhmPrimGAINCORRE[is,], yplus=fwhmPrimGAINCORRE[is,]+fwhmPrimGAINCORREErr[is,],
         #       type="n",cap=0, yminus=fwhmPrimGAINCORRE[is,]-fwhmPrimGAINCORREErr[is,],add=TRUE,errbar.col=colores[is])
@@ -76,8 +80,17 @@ for (id in 1:length(detectSPdirs)){
         labels[is] <- paste("Separation ",seps[is], " samples", sep="")
     }# separations [is]
 }# detection of Secondaries [id]
-legend("topleft",legend=labels, col=colores, cex=0.6, bty="n", lty=1)
+legend("bottomright",legend=labels, col=colores, cex=0.6, bty="n", lty=1)
+par(fig = c(0.07,0.5, 0.45, 0.95), new = T)
+plot(secEnergies,seq(0.,0.2,length.out = length(secEnergies)), type="n", xlab="", ylab="",cex.axis=0.5,
+     xlim=c(0.2,1), ylim=c(0,0.15))
+for (is in 1:length(seps)){
+    lines(secEnergies, diffPairSingle[is,], col=colores[is],lty=id, typ="b",cex=0.2)
+}
 
+    
+dev.off()
+        
 
 #################################
 
@@ -91,11 +104,11 @@ MinimDist <- matrix(data=c(10,60,60, 10,45,45, 10,45,45),
                     nrow=length(secEnergies), ncol=length(primEnergies), byrow = T)
 dimnames(MinimDist) <- list(c("sec02", "sec3", "sec7"), c("prim02", "prim3", "prim6"))
 
-plot(secEnergies, seq(5,60, length.out=length(secEnergies)),xlab="Energy of Secondaries (keV)",
-     ylab="Closest pulses for detection (samples)", type="n")
-for (ie in 1:length(primEnergies)){
-    lines(secEnergies, MinimDist[,ie],col=colsPrim[ie], lty=ie, type="b")
-    text(primEnergies[ie],MinimDist[,ie]+5,paste("Eprim=",primEnergies[ie]," keV",sep=""), col=colsPrim[ie], cex=0.8)
-}
+# plot(secEnergies, seq(5,60, length.out=length(secEnergies)),xlab="Energy of Secondaries (keV)",
+#      ylab="Closest pulses for detection (samples)", type="n")
+# for (ie in 1:length(primEnergies)){
+#     lines(secEnergies, MinimDist[,ie],col=colsPrim[ie], lty=ie, type="b")
+#     text(primEnergies[ie],MinimDist[,ie]+5,paste("Eprim=",primEnergies[ie]," keV",sep=""), col=colsPrim[ie], cex=0.8)
+# }
 
 
