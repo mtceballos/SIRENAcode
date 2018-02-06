c7a3de22b109125d4bacca4bb4df586e8e94a4ce
Maite Ceballos <ceballos@ifca.unican.es>
2018-02-06 12:34:59 +0100

    Adapted for smaprate2 and jitter

--- ERESOL/PAIRS/simulPairsMultiE.py
+++ ERESOL/PAIRS/simulPairsMultiE.py
@@ -26,6 +26,8 @@ import shutil
 import sys
 import tempfile
 import numpy
+import auxpy
+from time import gmtime, strftime
 from subprocess import check_call, check_output, STDOUT
 from astropy.io import fits
 import xml.etree.ElementTree as ET
@@ -40,65 +42,56 @@ nSimPulses = 20000  # 10000 records = 10000 secondary pulses
 nSimPulses = 2000  # 1000 records = 1000 secondary pulses
 recordSeparation = 40000  # separation from secondary --> primary for next record
 
-simSIXTEdir = "/home/ceballos/INSTRUMEN/EURECA/testHarness/simulations/SIXTE"
-PAIRSdir = "/home/ceballos/INSTRUMEN/EURECA/ERESOL/PAIRS"
+simSIXTEdir = "/dataj6/ceballos/INSTRUMEN/EURECA/testHarness/simulations/SIXTE"
+PAIRSdir = "/dataj6/ceballos/INSTRUMEN/EURECA/ERESOL/PAIRS"
 XMLdir = os.environ["SIXTE"] + "/" + "share/sixte/instruments/athena/1469mm_xifu"
-# XMLfile = XMLdir + "/" + "xifu_baseline.xml"
-XMLfile = XMLdir + "/" + "xifu_detector_hex_baseline.xml"
+
 pixel = 1
 PreBufferSize = 1000
-
-XMLtree = ET.parse(XMLfile)
-XMLroot = XMLtree.getroot()
-for samplefreq in XMLroot.findall('samplefreq'):
-    samprate = samplefreq.get('value')
-
-tstart = 0.5/float(samprate)  # added to solve floating point inaccu. due to sampling rate (Christian's mail 31/03/2017)
-
-triggerTH = {'LPA1shunt': 50, 'LPA2shunt': 20}
+pulseLength = 8192  # only to calculate triggerSize
+# with these thresholds, 0.2keV pulses are triggered at 999, 0.5keV @999 or @1000 and larger pulses @10000 
+triggerTH = {'LPA1shunt': 50, 'LPA2shunt': 20} 
 
 
-def simulPairs(pixName, monoEkeV1, monoEkeV2, acbias):
+def simulPairs(pixName, monoEkeV1, monoEkeV2, acbias, samprate, jitter, sepsStr):
     """
     :param pixName: Extension name in the FITS pixel definition file (SPA*, LPA1*, LPA2*, LPA3*)
     :param monoEkeV1: Monochromatic energy (keV) of input first simulated pulses
     :param monoEkeV2: Monochromatic energy (keV) of input second simulated pulses
     :param acbias: Operating Current (AC if acbias=yes or DC if acbias=no)
+    :param samprate: Samprate value with respect to baseline of 156250 Hz: "" (baseline), "samprate2" (half_baseline)
+    :param jitter: jitter option ("" for no_jitter and "jitter" for jitter)
+    :param sepsStr: separations between pulses
     :return: files with simulated PAIRS
     """
 
-    global cwd, nSimPulses, XMLfile, pixel, PreBufferSize, simSIXTEdir, samprate, triggerTH, tstart
-    if monoEkeV1 == "0.5" or monoEkeV2 == "0.5":
-            triggerTH["LPA2shunt"] = 50
+    global cwd, nSimPulses, XMLdir, pixel, PreBufferSize, simSIXTEdir, triggerTH, pulseLength
+    # samprate
+    smprtStr = ""
+    if samprate == 'samprate2':
+        smprtStr = "_samprate2"
+    XMLfile = "/dataj6/ceballos/INSTRUMEN/EURECA/ERESOL/xifu_detector_hex_baselineNEWgrades" + smprtStr + ".xml"
+    XMLtree = ET.parse(XMLfile)
+    XMLroot = XMLtree.getroot()
+    for samplefreq in XMLroot.findall('samplefreq'):
+        samprate = samplefreq.get('value')
+    # added to solve floating point inaccu. due to sampling rate (Christian's mail 31/03/2017)
+    tstart = 0.5 / float(samprate)
+
+    # jitter
+    jitterStr = ""
+    offset = ""
+    if jitter == "jitter":
+        jitterStr = "_jitter"
+        offset = " offset=-1"
+
+    if monoEkeV1 == "0.5":
+            triggerTH["LPA2shunt"] = 50 #increase threshold so that they are all triggered @999
 
     tessim = "tessim" + pixName
     SIMFILESdir = PAIRSdir + "/" + tessim
-    PixTypeFile = "file:" + simSIXTEdir + "/newpixels.fits[" + pixName + "]"
-    sepsStr = ['0']
-    pulseLength = 0
-    if "SPA" in pixName:
-
-        sepsStr = ['00004', '00005', '00007', '00010', '00013', '00017', '00023', '00031', '00042', '00056', '00075',
-                   '00101', '00136', '00182', '00244', '00328', '00439', '00589', '00791', '01061', '01423', '01908']
-        pulseLength = 1024  # only to calculate triggerSize
-
-    elif "LPA1" in pixName:
-
-        sepsStr = ['00004', '00005', '00007', '00010', '00013', '00017', '00023', '00031', '00042', '00056', '00075',
-                   '00101', '00136', '00182', '00244', '00328', '00439', '00589', '00791', '01061', '01423', '01908'
-                   ]
-        pulseLength = 2048  # only to calculate triggerSize
-
-    elif ("LPA2" in pixName) or ("LPA3" in pixName):
-
-        #sepsStr = ['00004', '00006', '00008', '00011', '00044', '00060', '00090', '00100', '00120', '00200',
-        #           '00250', '00300', '00400', '00500', '00600', '00800', '01000', '01600', '02000']
-
-        sepsStr = ['{0:05d}'.format(member) for member
-                   in list(map(int, numpy.ndarray.tolist(numpy.logspace(numpy.log10(4), numpy.log10(2000), num=20))))]
-
-        sepsStr = ['00200', '00210', '00220', '00230', '00240', '00250', '00260', '00270', '00280', '00290', '00300']
-        pulseLength = 4096  # only to calculate triggerSize
+    # PixTypeFile = "file:" + simSIXTEdir + "/newpixels.fits[" + pixName + "]"  # pre-Feb 2018 telecon
+    PixTypeFile = "file:" + simSIXTEdir + "/newpix_full.fits[" + pixName + "]"
 
     for sepA in sepsStr:
         sep12 = int(sepA)
@@ -112,8 +105,10 @@ def simulPairs(pixName, monoEkeV1, monoEkeV2, acbias):
         simTime = nSimPulses/2. * triggerSizeTC/float(samprate)
         simTime = '{0:0.0f}'.format(simTime)
 
-        root0 = "sep" + sepA + "sam_" + simTime + "s_" + monoEkeV1 + "keV_" + monoEkeV2 + "keV"  # for piximpact
-        root = "sep" + sepA + "sam_" + str(nSimPulses) + "p_" + monoEkeV1 + "keV_" + monoEkeV2 + "keV"  # for fits
+        # for piximpact:
+        root0 = "sep" + sepA + "sam_" + simTime + "s_" + monoEkeV1 + "keV_" + monoEkeV2 + "keV" + smprtStr + jitterStr
+        # for fits:
+        root = "sep" + sepA + "sam_" + str(nSimPulses) + "p_" + monoEkeV1 + "keV_" + monoEkeV2 + "keV" + smprtStr + jitterStr
         pixFile = cwd + "/PIXIMPACT/" + root0 + "_trSz" + str(triggerSizeTC) + ".piximpact"
         fitsFile = SIMFILESdir + "/" + root + ".fits"
         print("-------------------------------------------\n")
@@ -122,7 +117,7 @@ def simulPairs(pixName, monoEkeV1, monoEkeV2, acbias):
 
         if not os.path.isfile(pixFile):
             comm = ("tesconstpileup PixImpList=" + pixFile + " XMLFile=" + XMLfile + " timezero=" + str(tstart) +
-                    " tstop=" + str(simTime) + " energy=" + monoEkeV1 + " energy2=" + monoEkeV2 +
+                    " tstop=" + str(simTime) + " energy=" + monoEkeV1 + " energy2=" + monoEkeV2 + offset +
                     " pulseDistance=" + str(sep12) + " TriggerSize=" + str(triggerSizeTC) + " clobber=yes")
             print("\n##### Runing tesconstpileup #########")
             print(comm, "\n")
@@ -136,16 +131,19 @@ def simulPairs(pixName, monoEkeV1, monoEkeV2, acbias):
                 raise
             # continue  # to simulate only piximpact files
 
+
+
         if not os.path.isfile(fitsFile):
-            comm = ("tessim PixID=" + str(pixel) + " PixImpList=" + pixFile + " Streamfile=" + fitsFile + " tstart=0." +
-                    " tstop=" + simTime + " triggerSize=" + str(triggerSizeTS) + " preBuffer=" +
-                    str(PreBufferSize) + " triggertype='diff:3:" + str(triggerTH[pixName]) + ":" + str(triggerTS3val) +
-                    "'" + " acbias=" + acbias + " sample_rate=" + samprate + " PixType=" + PixTypeFile)
+            commTessim = ("tessim PixID=" + str(pixel) + " PixImpList=" + pixFile + " Streamfile=" + fitsFile +
+                          " tstart=0." + " tstop=" + simTime + " triggerSize=" + str(triggerSizeTS) + " preBuffer=" +
+                          str(PreBufferSize) + " triggertype='diff:3:" + str(triggerTH[pixName]) + ":" +
+                          str(triggerTS3val) + "'" + " acbias=" + acbias + " sample_rate=" + samprate +
+                          " PixType=" + PixTypeFile)
 
             print("\n##### Runing tessim #########")
-            print(comm, "\n")
+            print(commTessim, "\n")
             try:
-                args = shlex.split(comm)
+                args = shlex.split(commTessim)
                 check_call(args, stderr=STDOUT)
             except:
                 print("Error running TESSIM for data simulation")
@@ -153,6 +151,9 @@ def simulPairs(pixName, monoEkeV1, monoEkeV2, acbias):
                 shutil.rmtree(tmpDir)
                 raise
             # continue
+            # update HISTORY  for tesconstpileup run in header[0]
+            auxpy.updateHISTORY(fitsFile, commTessim)
+
             # rm first (and LAST) record and update NETTOT
             fsim = fits.open(fitsFile)
             nrows = fsim[1].header["NAXIS2"]
@@ -160,19 +161,8 @@ def simulPairs(pixName, monoEkeV1, monoEkeV2, acbias):
             fsim.close()
             # continue
             try:
-                print("Removing first & last row, just in case, and updating NETTOS")
-                comm = "fdelrow infile=" + fitsFile + "+1 firstrow=" + str(nrows) + " nrows=1 confirm=no proceed=yes"
-                args = shlex.split(comm)
-                check_call(args, stderr=STDOUT)
-                comm = "fdelrow infile=" + fitsFile + "+1 firstrow=1 nrows=1 confirm=no proceed=yes"
-                args = shlex.split(comm)
-                check_call(args, stderr=STDOUT)
-                fsim = fits.open(fitsFile, mode='update')
-                nrows2 = fsim[1].header['NAXIS2']
-                assert nrows2 == nrows-2, "Failure removing initial & last rows in (%s): " % fitsFile
-                nettot = nrows2 * 2  # new number of pulses
-                fsim[1].header['NETTOT'] = nettot
-                fsim.close()
+                print("Removing first & last row, just in case, and updating NETTOT")
+                auxpy.rmLastAndFirst(fitsFile, 2)
             except:
                 print("Error running FTOOLS to remove initial & last rows in ", fitsFile)
                 os.chdir(cwd)
@@ -194,7 +184,13 @@ if __name__ == "__main__":
     parser.add_argument('--monoEnergy2', help='Monochromatic energy (keV) of input simulated second pulse')
     parser.add_argument('--acbias', choices=['yes', 'no'], default="yes",
                         help='Operating Current (acbias=yes for AC or acbias=no for DC)')
-
+    parser.add_argument('--samprate', default="", choices=['', 'samprate2'], help="baseline, half_baseline")
+    parser.add_argument('--jitter', default="", choices=['', 'jitter'], help="no jitter, jitter")
+    parser.add_argument('--separations', help='spaced list of separations between Prim & Sec pulses', required=True,
+                        nargs='+')
     inargs = parser.parse_args()
+    assert isinstance(inargs.separations, object)
     simulPairs(pixName=inargs.pixName, monoEkeV1=inargs.monoEnergy1,
-               monoEkeV2=inargs.monoEnergy2, acbias=inargs.acbias)
+               monoEkeV2=inargs.monoEnergy2, acbias=inargs.acbias,
+               samprate=inargs.samprate, jitter=inargs.jitter,
+               sepsStr=inargs.separations)
