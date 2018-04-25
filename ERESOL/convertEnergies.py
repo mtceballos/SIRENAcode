c2e4eac5240f10cb5441fbb7a003096174fc5abe
Maite Ceballos <ceballos@ifca.unican.es>
2018-04-25 18:19:24 +0200

    Adapt to spyder

--- ERESOL/convertEnergies.py
+++ ERESOL/convertEnergies.py
@@ -7,7 +7,7 @@ python getEresolCurves_manySeps.py
         =======================================================
         ** Check resultsDir (nodetSP in case secondaries are not detected)
                             (detSP in case secondaries are also detected
-                            gainScale in case results are for gain scale curves)
+                        gainScale in case results are for gain scale curves)
         ** Check library (LONG, SHORT) & filenames
         ** Check jitter
 
@@ -23,10 +23,14 @@ from astropy.io import fits
 
 def convertEnergies(inFile, outFile, coeffsFile, alias):
     """
-    :param inFile: absolute path to file with reconstructed (non-calibrated) energies
-    :param coeffsFile: file with coefficients of polynomial fit to gain curves from polyfit2bias.R
-    :param alias: string to select reconstruction type in the coefficients table
-    :param outFile: file with reconstructed/calibrated energies for the input pulses
+    :param inFile: absolute path to file with reconstructed
+                    (non-calibrated) energies
+    :param coeffsFile: file with coefficients of polynomial
+                    fit to gain curves from polyfit2bias.R
+    :param alias: string to select reconstruction type in
+                    the coefficients table
+    :param outFile: file with reconstructed/calibrated energies
+                    for the input pulses
     """
 
     # ------------------------------------
@@ -42,7 +46,7 @@ def convertEnergies(inFile, outFile, coeffsFile, alias):
     EreconKeV = np.array(ftab['SIGNAL'])
 
     # calculate corrected energies with polyfit coeffs
-    print("...Calculating corrected energies for pulses in ",inFile)
+    print("...Calculating corrected energies for pulses in ", inFile)
     EcorrKeV = auxpy.enerToCalEner(EreconKeV, coeffsFile, alias)
 
     # close input file
@@ -55,7 +59,8 @@ def convertEnergies(inFile, outFile, coeffsFile, alias):
     shutil.copy(inFile, outFile)
     f = fits.open(outFile, memmap=True, mode='update')
     hdr = f[0].header
-    hdr['HISTORY'] = "Updated by convertEnergies.py to correct energies by gainscale"
+    hdr['HISTORY'] = ("Updated by convertEnergies.py to correct "
+                      "energies by gainscale")
     ftab = f[1].data
     ftab['SIGNAL'] = EcorrKeV
     f.close()
@@ -70,20 +75,25 @@ def convertEnergies(inFile, outFile, coeffsFile, alias):
 if __name__ == "__main__":
     import argparse
 
-    parser = argparse.ArgumentParser(description='Convert input energies to calibrated energies according to gainScale',
+    parser = argparse.ArgumentParser(description='Convert input energies to \
+                                     calibrated according to gainScale',
                                      prog='convertEnergies.py')
 
     parser.add_argument('--inFile', required=True,
-                        help='Input FITS file (EVENTS) with reconstructed energies')
+                        help='Input FITS file (EVENTS) with \
+                        reconstructed energies')
     parser.add_argument('--outFile', required=True,
-                        help='Output FITS file (EVENTS) with calibrated energies')
-    parser.add_argument('--coeffsFile', help='file with polynomial fit coefficients from R script polyfit2bias.R',
-                        default='')
-    parser.add_argument('--alias', help='String for reconstruction method in coefficient fuile '
-                                        '(i.e. fixedlib6OF_OPTFILT4096', required=True)
+                        help='Output FITS file (EVENTS) with \
+                        calibrated energies')
+    parser.add_argument('--coeffsFile', help='file with polynomial fit \
+                        coefficients from R script polyfit2bias.R', default='')
+    parser.add_argument('--alias', help='String for reconstruction method \
+                        in coefficient file (i.e. fixedlib6OF_OPTFILT4096)',
+                        required=True)
 
     inargs = parser.parse_args()
 
     # print("array=",inargs.array)
 
-    convertEnergies(inFile=inargs.inFile, outFile=inargs.outFile, coeffsFile=inargs.coeffsFile, alias=inargs.alias)
+    convertEnergies(inFile=inargs.inFile, outFile=inargs.outFile,
+                    coeffsFile=inargs.coeffsFile, alias=inargs.alias)
